c95a3de0cdbb462b8b8fcd43a395ef04493a4a2d
Ajay9433 <106583703+Ajay9433@users.noreply.github.com>
2022-06-03 13:28:26 +0530

    Update EvenNumber.java

--- EvenNumber.java
+++ EvenNumber.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 
 public class EvenNumber {
 public static void main(String[] args) {
-	int i,n;
+	int i,n;//declaring the variables
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the max. range: ");
-	n=sc.nextInt();
+	n=sc.nextInt();//taking input fom user
 	
-	for(i=1;i<=n;i++)
+	for(i=1;i<=n;i++)//implimenting condition to get even vlue
 	{
 		if(i%2==0)
 		{
 		System.out.println(i);
 		}
+		
+		//another way to find same thing
 		/*
 		 for(i=2;i<=50;i=i+2)
 		 {
